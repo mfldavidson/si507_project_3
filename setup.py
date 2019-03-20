33ba98b38f9c0bbca688f983188f990bd77d22a4
Maggie Davidson <jmagdavidson@gmail.com>
2019-03-20 09:08:31 -0400

    Renamed models to setup, refined structure

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
-# association tables
-movie-rating = db.Table('movie-rating',db.Column('movie_id',db.Integer, db.ForeignKey('movies.id')),db.Column('rating_id',db.Integer, db.ForeignKey('ratings.id')))
+from flask import Flask, render_template, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-movie-distributor = db.Table('movie-distributor',db.Column('movie_id',db.Integer, db.ForeignKey('movies.id')),db.Column('distributor_id',db.Integer, db.ForeignKey('distributors.id')))
+# Application configurations
+app = Flask(__name__)
+app.debug = True
+app.use_reloader = True
+app.config['SECRET_KEY'] = 'gordonbdnumber1cat'
 
-movie-director = db.Table('movie-director',db.Column('movie_id',db.Integer, db.ForeignKey('movies.id')),db.Column('director_id',db.Integer, db.ForeignKey('directors.id')))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./movies.db'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) # For database use
+session = db.session # to make queries easy
 
 # models
 class Movie(db.Model):
     __tablename__ = "movies"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
-    rating = db.relationship('Rating',secondary=movie-rating,backref=db.backref('ratings',lazy='dynamic'),lazy='dynamic')
+    rating = db.Column(db.Integer, db.ForeignKey('ratings.id'))
     run_time = db.Column(db.Integer)
-    distributor = db.relationship('Distributor',db.backref('distributors',lazy='dynamic'),lazy='dynamic')
+    distributor = db.Column(db.Integer, db.ForeignKey('distributors.id'))
     genre = db.Column(db.String(24))
-    director = db.relationship('Director',secondary=movie-director,backref=db.backref('Director',lazy='dynamic'),lazy='dynamic')
+    director = db.Column(db.Integer, db.ForeignKey('directors.id'))
 
     def __repr__(self):
-        return '{} by director {}'.format(self.title,self.director)
+        return '{} which is rated {}'.format(self.title,Rating.query.filter_by(id=self.rating).first().text)
 
 class Rating(db.Model):
     __tablename__ = "ratings"
     id = db.Column(db.Integer, primary_key=True)
     num = db.Column(db.Integer)
     text = db.Column(db.String(64))
+    movies = db.relationship('Movie',backref='Rating')
 
     def __repr__(self):
         return "{} rating with number value {}".format(self.text,self.num)
@@ -32,31 +42,31 @@ class Distributor(db.Model):
     __tablename__ = "distributors"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
+    movies = db.relationship('Movie',backref='Distributor')
 
     def __repr__(self):
         return "{} (ID: {})".format(self.name,self.id)
 
 class Director(db.Model):
+    __tablename__ = "directors"
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(24))
     lname = db.Column(db.String(24))
+    movies = db.relationship('Movie',backref='Director')
 
     def __repr__(self):
         return "{} {}".format(self.fname,self.lname)
 
 # functions to get or create new values
-def get_or_create_rating(rating_text):
+def get_rating(rating_text,rating_num=None):
     rating = Rating.query.filter_by(text=rating_text).first()
     if rating:
         return rating
     else:
-        rating = Rating(text=rating_text)
-        session.add(rating)
-        session.commit()
-        return rating
+        return 'This is not a valid MPAA rating. Please use one of the following: G, PG, PG-13, R, or NC-17'
 
 def get_or_create_distributor(distributor_name):
-    distributor = Distributor.query.filter_by(name=distrdistributor_name).first()
+    distributor = Distributor.query.filter_by(name=distributor_name).first()
     if distributor:
         return distributor
     else:
